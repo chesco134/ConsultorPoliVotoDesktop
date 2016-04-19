51ac6ac9020a4d8a4d7b8c8e563d1729dc193028
chesco134 <chesco134@gmail.com>
2016-04-19 04:02:45 -0500

    Se agrega una propuesta de sincronización, aunque debe ser revisada, porque aunque corrge la propagación de un error, aún no se logra una verdadera sincronización.

---
+++ src/com/polivoto/threading/ServicioDeSincronizacionDeReloj.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.polivoto.threading;
+
+import com.polivoto.logica.Cronometro;
+import com.polivoto.networking.IOHandler;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.TimerTask;
+
+/**
+ *
+ * @author jcapiz
+ */
+public class ServicioDeSincronizacionDeReloj extends TimerTask {
+    
+    private String host;
+    private Cronometro cronometro;
+
+    public ServicioDeSincronizacionDeReloj(String host, Cronometro cronometro) {
+        this.host = host;
+        this.cronometro = cronometro;
+    }
+    
+    @Override
+    public void run(){
+        try{
+            Socket socket = new Socket(host, 33059);
+            DataInputStream entrada = new DataInputStream(socket.getInputStream());
+            DataOutputStream salida = new DataOutputStream(socket.getOutputStream());
+            IOHandler ioHandler = new IOHandler(entrada, salida);
+            long currentTime = ioHandler.readLong();
+            cronometro.setTiempoRestante(currentTime);
+            if(currentTime <= 0) super.cancel();
+            System.out.println("Reajuste de tiempo: " + currentTime);
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+}
